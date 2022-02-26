50e2243c8e3ad32c1498290dd060580884385052
Faturrahman Ma'ruf <pedro@MacBook-Air-M1.local>
2022-02-26 20:36:49 +0700

    terraform fmt

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "3.72.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.1.0"
     }
   }
   required_version = ">= 1.1.0"
@@ -28,7 +28,7 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
